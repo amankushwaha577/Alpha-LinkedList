8a6c9d219a4df1696ea4925445c64eb803a33087
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-29 18:59:42 +0530

    findMiddle

---
+++ _15 LL Find the middle element of the LinkedList/src/Optimal.java
@@ -0,0 +1,116 @@
+
+class CustomLL2 {
+    Node head; // Head of the linked list
+
+    // Definition of a Node in the linked list
+    class Node {
+        int data;      // Data stored in the node
+        Node next;     // Pointer to the next node
+
+        // Constructor to initialize a node
+        public Node(int data1) {
+            this.data = data1;  // Set the data
+            this.next = null;   // Initialize next as null
+        }
+    }
+
+    // Method to add a node with the given data at the end of the list
+    public void addLast(int data) {
+        Node newNode = new Node(data); // Create a new node
+        if (head == null) {
+            head = newNode; // If the list is empty, make the new node the head
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) { // Traverse to the last node
+            currNode = currNode.next;
+        }
+        currNode.next = newNode; // Attach the new node at the end
+    }
+
+    // Method to print the linked list
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) { // Traverse and print each node
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("null"); // End of the list
+    }
+
+    // Function to find the middle node of a linked list
+    static Node findMiddle(Node head) { // T:0(N1) for length check + 0(N/2) for traverse up to mid || S:0(1)
+        // If the list is empty or has
+        // only one element, return the head as
+        // it's the middle.
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node temp = head;
+        int count = 0;
+
+        // Count the number of nodes
+        // in the linked list.
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        // Calculate the position of the middle node.
+        int mid = count / 2 + 1;  // if count = 5 => (5/2)+1 = 2+1 =3 Great || count = 6 => (6/2)+1 = 3+1 =4 Great ( Choose Right Middle )
+        temp = head;
+
+        while (temp != null) {
+            mid--;
+
+            // Check if the middle
+            // position is reached.
+            if (mid == 0){
+                // break out of the loop
+                // to return temp
+                break;
+            }
+            // Move temp ahead
+            temp = temp.next;
+        }
+
+        // Return the middle node.
+        return temp;
+    }
+
+}
+
+// Main class to test the CustomLL1 class and findMiddle method
+public class Optimal {
+    public static void main(String[] args) {
+        // Create a linked list
+        CustomLL2 list = new CustomLL2();
+
+        // Adding sample nodes to the list
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addLast(5);
+
+        // Print the linked list
+        System.out.println("List:");
+        list.printList();
+
+        // Find the middle node
+        CustomLL2.Node middleNode = CustomLL2.findMiddle(list.head);
+
+        // Print the middle node
+        if (middleNode != null) {
+            System.out.println("Middle node is: " + middleNode.data);
+        } else {
+            System.out.println("The list is empty.");
+        }
+    }
+}
+
