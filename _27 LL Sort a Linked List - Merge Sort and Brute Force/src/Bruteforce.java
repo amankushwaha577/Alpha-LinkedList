c4af06947349c195d6addee5c65c9be764269c0e
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-12-31 22:29:40 +0530

    mergesort

---
+++ _27 LL Sort a Linked List - Merge Sort and Brute Force/src/Bruteforce.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class CustomLL {
+    Node head; // Head of the linked list
+
+    // Definition of a Node in the linked list
+    static class Node {
+        int data;      // Data stored in the node
+        Node next;     // Pointer to the next node
+
+        // Constructor to initialize a node
+        public Node(int data1) {
+            this.data = data1;  // Set the data
+            this.next = null;   // Initialize next as null
+        }
+    }
+
+    // Method to add a node with the given data at the end of the list
+    public void addLast(int data) {
+        Node newNode = new Node(data); // Create a new node
+        if (head == null) {
+            head = newNode; // If the list is empty, make the new node the head
+            return;
+        }
+        Node currNode = head;
+
+        // Traverse to the last node in the list
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+
+        currNode.next = newNode; // Attach the new node at the end
+    }
+
+    // Function to sort a linked list using the Brute Force approach
+    public void sortLL() {
+        // Create a list to store node values
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        // Temporary pointer to traverse the linked list
+        Node temp = head;
+
+        // Traverse the linked list and store node values in the list
+        while (temp != null) {
+            arr.add(temp.data);
+            temp = temp.next;
+        }
+
+        // Sort the list containing node values
+        Collections.sort(arr);
+
+        // Reassign sorted values to the linked list nodes
+        temp = head;
+        for (int i = 0; i < arr.size(); i++) {
+            temp.data = arr.get(i);
+            temp = temp.next;
+        }
+    }
+
+    // Method to print the linked list
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+}
+
+public class Bruteforce {
+    public static void main(String[] args) {
+        // Create a linked list and add some elements
+        CustomLL list = new CustomLL();
+        list.addLast(4);
+        list.addLast(2);
+        list.addLast(5);
+        list.addLast(1);
+        list.addLast(3);
+
+        System.out.println("Original List:");
+        list.printList();
+
+        // Sort the linked list using Brute Force
+        list.sortLL();
+
+        System.out.println("Sorted List:");
+        list.printList();
+    }
+}
